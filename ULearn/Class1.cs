cec4c5d9c539fe0dcaf45e25b09327198c56425a
endsinger <102259704+endsinger@users.noreply.github.com>
2022-06-24 18:06:15 +0400

    дискриминант
    дискриминант

---
+++ ULearn/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULearn
+{
+    class Class1
+    {
+        public static void GetDiscriminant(double a, double b, double c)
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+            Console.WriteLine($"Дискриминант равен: {d}");
+            
+            if (d < 0)
+            {
+                Console.WriteLine("корней нет");
+            } 
+            else if (d == 0){
+                double ifZero = -b / (2 * a);
+                Console.WriteLine("Один корень равный: " + ifZero);
+            } 
+            else if (d > 0)
+            {
+                double x1 = (-b - Math.Sqrt(d)) / (2 * a);
+                x1 = Math.Round(x1, 3);
+                double x2 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = Math.Round(x2, 3);
+                Console.WriteLine($"Первый корень равен: {x1}, второй корень равен: {x2}");
+            }
+        }
+
+    }
+}
---
+++ ULearn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ULearn
+{
+    class Program
+    {      
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("========================== \n \n Программа решения квадратного уравнения \n Введиет значения переменных a, b, c");
+                try
+                {
+                    double a = double.Parse(Console.ReadLine());
+                    double b = double.Parse(Console.ReadLine());
+                    double c = double.Parse(Console.ReadLine());
+                    Class1.GetDiscriminant(a, b, c);
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка, введеные данные некорректны");
+                }
+            }
+        }
+
+    }
+
+}
